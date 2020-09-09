c454fce0e264058a0447ee880c2c1f98df239a84
Brandon Marcos Kluck <brandon.kluck@neomind.com.br>
2020-09-09 19:35:08 -0300

    Criação das rotas de criação, reset password e login

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -6,12 +6,33 @@ const UserController = require('../controllers/UserController');
 const router = express.Router();
 
 router.get('/users', UserController.getAllUsers);
-router.post('/users',  celebrate({
+router.post('/create_users',  celebrate({
     [Segments.BODY]: Joi.object().keys({
         name: Joi.string().required().trim(),
         email: Joi.string().required().email().trim(),
+        password: Joi.string().required().min(6).max(12),
         phone: Joi.string().required().min(11).max(15).trim(),
     })
 }), UserController.create);
+
+router.post('/request_password_user',  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        email: Joi.string().required().email().trim(),
+    })
+}), UserController.requestResetPassword);
+
+router.put('/reset_password_user',  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        email: Joi.string().required().email().trim(),
+        password: Joi.string().required(),
+    })
+}), UserController.resetPassword);
+
+router.post('/authenticate',  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        email: Joi.string().required().email().trim(),
+        password: Joi.string().required().trim(),
+    })
+}), UserController.authenticate);
 
 module.exports = router;
