6ac2d5c3f851190b9c1f9ba4d1a546001c489419
dayanFreitas <dayan.freitas@catolicasc.org.br>
2020-09-13 21:42:38 -0300

    script of create front end

---
+++ frontend/bin/make.sh
@@ -0,0 +1,63 @@
+# make.sh nome_do_arquivo
+# make.sh -h[help]
+
+raiz=$(pwd)
+if [ $# -eq 0 ]
+then
+    echo 'Passe o nome do arquivo';
+    echo '  Exemplo: make.sh NomeDoArquivo';
+    return 0;
+fi
+
+echo 'Make::'$1
+cd frontend/src/pages
+mkdir $1
+cd $1
+
+## Criando js
+echo "import React, { Component, Fragment } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Loader from '../../component/Loader'
+import '/./$1.css'
+
+class $1 extends Component {
+    stateInitial = {
+        loading: false  
+    }
+    
+    state = this.stateInitial
+    
+    clearState = () => {
+        this.setState(this.stateInitial)
+    }
+
+    render () {
+      let template = 'Teste'
+
+        return (
+            <Fragment>
+                <Loader loading={this.state.loading}/>
+                <Container className=\"$1\">
+                    <Row>
+                        {template}
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+
+}
+
+export default $1" >> $1.js
+
+# Criando Css
+echo ".$1{}" >> $1.css
+
+
+cd $raiz
+pwd
+cd frontend/src
+
+#Fazer o replace das novas rotas do front end
+sed -i "s/RouterGeneric/<Route path=\"$1\" component={$1} \/>\\nRouterGeneric/gi" routes.js  
+sed -i "s/\/\/ImportRouter/import $1 from '.\/pages\/$1\/$1'\\n\\/\\/ImportRouter/gi" routes.js  
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login/login';
 import ForgotPassword from './pages/forgot-password/forgotPassword';
+//ImportRouter
 
 export default function Routes () {
     return (
@@ -10,6 +11,7 @@ export default function Routes () {
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/forgot-password" component={ForgotPassword} />
+RouterGeneric
             </Switch>
         </BrowserRouter>
     )
