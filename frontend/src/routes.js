0e57b12c698d831e552e1255fb37f6eabb6e96f8
dayanFreitas <dayan.freitas@catolicasc.org.br>
2020-09-13 23:54:34 -0300

    form user

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import Login from './pages/Login/login';
+import UserRegistration from './pages/UserRegistration/UserRegistration'
+//ImportRouter
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login}/>
+                <Route path="/UserRegistration" component={UserRegistration} />
+RouterGeneric
             </Switch>
         </BrowserRouter>
     )
