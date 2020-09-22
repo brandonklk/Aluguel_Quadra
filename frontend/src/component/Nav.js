544a194914b88b436ac0e61178698a0251328215
dayanFreitas <dayan.freitas@catolicasc.org.br>
2020-09-21 22:30:58 -0300

    Navegador geneico

---
+++ frontend/src/component/Nav.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react'
+import { useHistory } from "react-router-dom"
+import { Image, Navbar, NavDropdown } from 'react-bootstrap'
+import ActionsUserRegistration from '../actions/UserRegistration/UserRegistration'
+import logoImg from '../assets/logo.png'
+
+export default function Nav (props) {
+    let history = useHistory()
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        ActionsUserRegistration.getUserById(props.id)
+        .then((r) => {
+            setUser(r)
+        })
+    }, []);
+    
+    const logoutUser = () => {
+        props.logout() 
+        history.push("/");
+    }
+
+    const style = {
+        navStyle: {
+            'background': '#9ccc65',
+            // position: fixed;
+            // width: 100vw;
+            // top: 0;
+            // z-index: 2;
+        },
+        img: {
+            'box-shadow': '0px 0px 22px white'
+        },
+        nameTitle: {
+            'font-style': 'italic',
+            'font-size': '1.5em'
+        }
+    }
+
+    return (
+        <Navbar style={style.navStyle}>
+            <Navbar.Brand>
+            <img
+                src={logoImg}
+                width="80"
+                height="80"
+                className="d-inline-block align-top"
+                alt="Logo"
+            />
+            </Navbar.Brand>
+
+            <span className="user-name" style={style.nameTitle}>Aluguel de quadras</span>
+            
+            <Navbar.Collapse className="justify-content-end">
+                <Image src={user.image_base_64} roundedCircle height="60" width="60" style={style.img}/><br/>
+                <NavDropdown title={user.name} id="collasible-nav-dropdown">
+                <NavDropdown.Item onClick={logoutUser}>Sair</NavDropdown.Item>
+            </NavDropdown>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,34 +1,41 @@
 import React from 'react';
-import { isAuthenticated, logout } from './services/auth'
+import { isAuthenticated, logout, getToken } from './services/auth'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Nav from './component/Nav'
+
 import Login from './pages/Login/login';
 import UserRegistration from './pages/UserRegistration/UserRegistration'
 import ForgotPassword from './pages/forgot-password/forgotPassword';
 import Dashboard from './pages/Dashboard/Dashboard'
+import TennisCourts from './pages/TennisCourts/TennisCourts'
 //ImportRouter
 
 const PrivateRouter = ({ component: Component, ...rest}) => (
-    <Route 
-        {...rest} 
-        render={ props => (
-            isAuthenticated() ? (
-                <Component {...props} />
-            ) : (
-                <Redirect to={{pathname: '/', state: {form: props.location} }} />
-            )
-        )} 
-    />
+    <div>
+        <Nav id={getToken()} logout={logout}/>
+        <Route 
+            {...rest} 
+            render={ props => (
+                isAuthenticated() ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{pathname: '/', state: {form: props.location} }} />
+                )
+            )} 
+        />
+    </div>
 )
 
 
 const Routes = () => (
     <BrowserRouter>
-        <Switch>isAuthenticated
+        <Switch>
             <Route path="/" exact component={Login}/>
             <Route path="/UserRegistration" component={UserRegistration} />
             <Route path="/forgot-password" component={ForgotPassword} />
             <PrivateRouter path="/Dashboard" component={Dashboard}/>
-            RouterGeneric
+            <PrivateRouter path="/TennisCourts" component={TennisCourts} />
+				RouterGeneric
             </Switch>
     </BrowserRouter>
 )
