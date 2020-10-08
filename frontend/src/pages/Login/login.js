4ea860e47fac6511ccf334a5b0070b4dd5b82d40
dayanFreitas <dayan.freitas@catolicasc.org.br>
2020-10-07 22:13:10 -0300

    layout novo

--- frontend/src/pages/Login/login.js
+++ frontend/src/pages/Login/login.js
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { Fragment , useState } from "react";
 import { useHistory } from "react-router-dom"
 import { Container, Row, Col, Alert } from 'react-bootstrap';
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 
 import './login.css';
-import Loader from '../../component/Loader'
+import Footer from "../../component/Footer";
+import Loader from '../../component/Loader';
 import logoImg from '../../assets/logo.png';
 
-import Actions from "../../actions/Authenticate/Authenticate"
+import Actions from "../../actions/Authenticate/Authenticate";
 import { login, saveUser } from "../../services/auth";
+
+
 
 const Login = (props) => {
   const {param} = props.location
@@ -61,47 +64,50 @@ const Login = (props) => {
   }
 
   return (
-    <form onSubmit={formik.handleSubmit} className={!formik.isValid ? 'not-valid' : ''} autocomplete="off">
-      <Loader loading={loading}/>
-      <Container>
-        <Alert/>
-        <Row className="justify-content-md-center">
-          <Col xl="6">
-          <img src={logoImg} alt="Logo quadra" className="img-logo" width="150" height="150"/>
-            <h1 className="title">Login</h1>
-            
-            {error && <Alert variant="danger">{error}</Alert>} 
+    <Fragment>
 
-            <input type="text"
-                   placeholder="Email"  
-                   name="email"
-                   onChange={formik.handleChange}
-                   value={formik.values.email}
-            />
-            {formik.touched.email && formik.errors.email 
-              ? <div className="feed-back-error-input">{formik.errors.email}</div>
-              :''
-            }
+      <form onSubmit={formik.handleSubmit} className={!formik.isValid ? 'not-valid' : ''} autocomplete="off">
+        <Loader loading={loading}/>
+        <Container>
+          <Alert/>
+          <Row className="justify-content-md-center">
+            <Col xl="6">
+            <img src={logoImg} alt="Logo quadra" className="img-logo" width="150" height="150"/>
+              <h1 className="title">Login</h1>
+              
+              {error && <Alert variant="danger">{error}</Alert>} 
 
-            <input type="password"  
-                   className="mt-2" 
-                   placeholder="Senha" 
-                   name="password"
-                   onChange={formik.handleChange}
-                   value={formik.values.password}
-            />
-            {formik.touched.password && formik.errors.password 
-              ? <div className="feed-back-error-input">{formik.errors.password}</div>
-              :''
-            }
-            
-            <button variant="dark" className="mt-4 full" type="submit">Entrar</button>
-            <button variant="link" className="mt-4 full" type="button" onClick={goViewForgotPassword}>Esqueceu a senha?</button>            
-            <button variant="mostarda" align-item="center" className="mt-4 full" size="sm" type="button" onClick={() => {history.push('/UserRegistration')}}>Criar Nova Conta</button>
-          </Col>
-        </Row>
-      </Container>
-    </form>
+              <input type="text"
+                    placeholder="Email"  
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+              />
+              {formik.touched.email && formik.errors.email 
+                ? <div className="feed-back-error-input">{formik.errors.email}</div>
+                :''
+              }
+
+              <input type="password"  
+                    className="mt-2" 
+                    placeholder="Senha" 
+                    name="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+              />
+              {formik.touched.password && formik.errors.password 
+                ? <div className="feed-back-error-input">{formik.errors.password}</div>
+                :''
+              }
+              
+              <button variant="dark" className="mt-4 full" type="submit">Entrar</button>
+              <button variant="link" className="mt-4 full" type="button" onClick={goViewForgotPassword}>Esqueceu a senha?</button>            
+              <button variant="mostarda" align-item="center" className="mt-4 full" size="sm" type="button" onClick={() => {history.push('/UserRegistration')}}>Criar Nova Conta</button>
+            </Col>
+          </Row>
+        </Container>
+      </form>
+    </Fragment>
   )
   
 }
