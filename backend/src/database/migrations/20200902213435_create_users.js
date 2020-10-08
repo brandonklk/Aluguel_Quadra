7425c9f7b0f559d61a5fcc2770b3bb926d23571e
israelmurilocadorin <israel.cadorin@catolicasc.org.br>
2020-10-07 23:00:53 -0300

    user permission

--- backend/src/database/migrations/20200902213435_create_users.js
+++ backend/src/database/migrations/20200902213435_create_users.js
@@ -1,6 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
       table.increments('id');
+      table.boolean('permission').defaultTo(false);
       table.string('name').notNullable();
       table.string('email').unique().notNullable();
       table.string('passwordHash').notNullable();
--- backend/src/database/seeds/01-user.js
+++ backend/src/database/seeds/01-user.js
@@ -7,11 +7,12 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, name: 'dayan', email: 'dayan@gmail.com', passwordHash: senha, phone:'11111111111'},
-        {id: 2, name: 'israel', email: 'israel@gmail.com', passwordHash: senha, phone:'11111111111'},
-        {id: 3, name: 'brandon', email: 'brandon@gmail.com', passwordHash: senha, phone:'11111111111'},
-        {id: 4, name: 'willian', email: 'willian@gmail.com', passwordHash: senha, phone:'11111111111'},
-        {id: 5, name: 'dany', email: 'dany@gmail.com', passwordHash: senha, phone:'11111111111'}
+        {id: 1, permission: false, name: 'dayan', email: 'dayan@gmail.com', passwordHash: senha, phone:'11111111111'},
+        {id: 2, permission: false, name: 'israel', email: 'israel@gmail.com', passwordHash: senha, phone:'11111111111'},
+        {id: 3, permission: false, name: 'brandon', email: 'brandon@gmail.com', passwordHash: senha, phone:'11111111111'},
+        {id: 4, permission: false, name: 'willian', email: 'willian@gmail.com', passwordHash: senha, phone:'11111111111'},
+        {id: 5, permission: false, name: 'dany', email: 'dany@gmail.com', passwordHash: senha, phone:'11111111111'},
+        {id: 6, permission: true, name: 'suporte', email: 'suporte@gmail.com', passwordHash: senha, phone:'11111111111'}
       ]);
     });
 };
